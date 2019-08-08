b0c341ecf3da84a6712ce60ae5a2c0dae37acaef
Brian Wong <bwong92@gmail.com>
2019-08-08 04:05:56 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,14 +15,13 @@ class String
   end
 
   def count_sentences
-  #   sentences = self.split(/[.!?]/)
-  #   sentence_arr = []
-  #   sentences.each do |sent|
-  #     sentence_arr << sent
-  #   end
-  #   sentence_arr.uniq.count
-  # end
-  self.split(/[.!?]/).reject {|x| x.empty?}.count
-binding.pry
+    sentences = self.split(/[.!?]/)
+    sentence_arr = []
+    sentences.each do |sent|
+      sentence_arr << sent
+    end
+    sentence_arr.reject { |x| x.empty?}.count
+  
+
   end
 end
